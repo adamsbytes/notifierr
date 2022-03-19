a9047c0dc86be697bc72bb9d9b17d63f29666f22
Adam <aquanne@gmail.com>
2022-03-19 18:41:34 -0400

    fix: minor linting corrections

--- notifierr/sms/handler.py
+++ notifierr/sms/handler.py
@@ -1,8 +1,9 @@
 ''' The primary sms handler for the application '''
+import importlib
+
 class SMSHandler():
     ''' An agnostic interface to multiple SMS providers '''
     def __init__(self, provider) -> None:
-        import importlib
         provider_module = importlib.import_module(
             name=f'.providers.{provider}',
             package='sms'
@@ -11,8 +12,4 @@ class SMSHandler():
 
     def send_message(self, receiver, message) -> None:
         ''' Send an SMS message '''
-        # TODO: remove debug code before commit
-        #print(f'Message send activated for {receiver}')
-        #print(f'Message: {message}')
         self.client.send_message(receiver, message)
-        #print('Success???')
